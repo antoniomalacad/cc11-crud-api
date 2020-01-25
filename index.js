79e2db4f09ea0b181af85aec8c2d0f7ba972ad3f
Antonio <antoniomalacad@gmail.com>
2020-01-25 21:24:58 +0900

    server setup

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const config = require("./config");
-const services = require("./services")(config);
 const knex = require("knex")("./knexfile.js");
-
+const morgan = require("morgan");
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(morgan("dev"));
+
+app.use(bodyParser.json({ type: "application/json", limit: "50mb" }));
+
 app.use(
   bodyParser.urlencoded({
     extended: true
   })
 );
+
+app.get("/", (request, response) => {
+  response.json({ info: "Using Node.js, Express, and Postgres API." });
+});
+
+app.listen(config.express.port, () => {
+  console.log(`Server up and listening on port ${config.express.port}`);
+});
